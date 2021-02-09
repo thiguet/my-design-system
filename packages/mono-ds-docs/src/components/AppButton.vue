9507c582f71b659b7a74ffcdc1be509872a28845
Thiguet <thiguet2014@gmail.com>
2021-02-08 21:24:27 -0300

    Added.

--- packages/mono-ds-docs/src/components/AppButton.vue
+++ packages/mono-ds-docs/src/components/AppButton.vue
@@ -1,30 +1,31 @@
 <template>
-	<button :style="styles" @click="handleClick">
-		<!-- @slot Use this slot to place the button content -->
-		<slot></slot>
-	</button>
+  <button :style="styles" @click="handleClick">
+    <!-- @slot Use this slot to place the button content -->
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-import sizeMixin from './sizeMixin';
+import sizeMixin from "./sizeMixin";
 
 export default {
-  name: 'app-button',
+  name: "app-button",
+
   mixins: [sizeMixin],
   props: {
     /**
-		 * Sets the button font color
-		 */
+     * Sets the button font color
+     */
     color: {
       type: String,
-      default: 'black',
+      default: "black",
     },
     /** Sets background color of the button
-		 * @since 1.2.0
-		 */
+     * @since 1.2.0
+     */
     background: {
       type: String,
-      default: 'white',
+      default: "gray",
     },
     /** @deprecated Use color instead */
     oldColor: String,
@@ -32,7 +33,7 @@ export default {
   computed: {
     styles() {
       return {
-        'font-size': this.size,
+        "font-size": this.size,
         color: this.color,
         background: this.background,
       };
@@ -41,16 +42,16 @@ export default {
   methods: {
     handleClick(e) {
       /** Triggered when button is clicked
-			 * @event click
-			 * @type {Event}
-			 */
-      this.$emit('click', e);
+       * @event click
+       * @type {Event}
+       */
+      this.$emit("click", e);
 
       /** Event for Alligator's example
-			 * @event gator
-			 * @type {Event}
-			 */
-      this.$emit('gator', e);
+       * @event gator
+       * @type {Event}
+       */
+      this.$emit("gator", e);
     },
   },
 };
