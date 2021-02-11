a6112b118cb3362f9b315e75aead6547c6ee5d47
Thiguet <thiguet2014@gmail.com>
2021-02-10 23:38:29 -0300

    fix: bug fix

---
+++ packages/@a-design-sys/mono-ds-tokens/__tests__/mono-ds-tokens.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const monoDsTokens = require('..');
+
+describe('mono-ds-tokens', () => {
+    it('needs tests');
+});
---
+++ packages/@a-design-sys/mono-ds-tokens/src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = monoDsTokens;
+
+function monoDsTokens() {
+    return {
+        padding: 10,
+    };
+}
